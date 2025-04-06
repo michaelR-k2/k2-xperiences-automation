ef95239ff59a0628a2cc4fdaee28bed886463f76
Michael Alejandro Rios Rodriguez <204783788+michaelR-k2@users.noreply.github.com>
2025-04-06 15:02:13 -0500

    trip Users tests added

--- e2e/pages/modules/trip_usersPage.js
+++ e2e/pages/modules/trip_usersPage.js
@@ -3,6 +3,23 @@ import { locator, page, expect } from "@playwright/test";
 export default class TripUsersPage {
   constructor(page) {
     this.page = page;
+    // 📄 Locators de la tabla
+    this.usersTable = page.locator("table tbody");
+    this.allUsers = this.usersTable.locator("tr");
+    this.tableHeaders = page.locator("table thead tr th div span");
+    // 🔄 Paginación
+    this.rowsPerPageButton = page.locator("button[role='combobox']");
+    this.paginationNext = page.locator("button[aria-label='Next page']");
+    this.paginationPrev = page.locator("button[aria-label='Previous page']");
+  }
 
+  async getUserCount() {
+    await this.page.waitForSelector("table tbody tr", {state: "attached"});
+    return await this.allUsers.count();
+    
+  }
+  async getTableHeaders() {
+    await this.tableHeaders.first().waitFor({ state: "visible" });
+    return await this.tableHeaders.allTextContents();
   }
 }
--- e2e/tests/modules/trip_users/trip_users.spec.js
+++ e2e/tests/modules/trip_users/trip_users.spec.js
@@ -8,8 +8,33 @@ let trip_usersPage;
 
 test.beforeEach(async ({ page, context }) => {
   await allure.epic("Trip Users - Casos de prueba relacionados a la vista de Usuarios de Viaje");
+  await allure.feature("Trip Users");
   await allure.suite("Trip_users");
   await allure.tags("e2e", "trip_users");
   basePage = new BasePage(page);
   trip_usersPage = new TripUsersPage(page);
 });
+
+test("@trip_users - Validar que la tabla de usuarios de Viajes tenga al menos un usuario", async ({page}) => {
+  await allure.story("Transacciones desplegadas en la tabla");
+  await allure.step(`Step 1 - Validación de Transacciones mostradas en la tabla`,async () => {
+    await page.goto(`${process.env.BASEURL}/trip-users`);
+      const userCount = await trip_usersPage.getUserCount();
+      expect(userCount).toBeGreaterThan(0);
+    }
+  );
+});
+
+test("@trip_users - Validar que la tabla contenga los encabezados correctos", async ({page}) => {
+  await allure.story("Encabezados en la tabla de usuarios de Viajes");
+  await allure.step(`Step 1 - Validación data desplegada en la tabla`,async () => {
+    await page.goto(`${process.env.BASEURL}/trip-users`);
+    const expectedHeaders = ["ID", "name", "Email", "Identification", "Address", "Country", "Phone", "Birthdate"];
+    const actualHeaders = await trip_usersPage.getTableHeaders();
+    expect(actualHeaders).toEqual(expectedHeaders);
+    }
+  );
+});
+
+// Pendiente de Casos de Prueba relacionados a Paginación 
+
