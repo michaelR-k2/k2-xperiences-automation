875f3dfe9fe24a14a3d8ca6bf4efc0649d35db69
Michael Alejandro Rios Rodriguez <204783788+michaelR-k2@users.noreply.github.com>
2025-04-09 00:09:39 -0500

    updating Vendors tests and pages

--- e2e/pages/modules/vendorsPage.js
+++ e2e/pages/modules/vendorsPage.js
@@ -3,6 +3,45 @@ import { locator, page, expect } from "@playwright/test";
 export default class VendorsPage {
   constructor(page) {
     this.page = page;
+    this.createVendorButton = page.locator('div.w-full.flex.flex-col.sm\\:flex-row.sm\\:items-center > button.app-button');
+    this.nameInput = page.locator('input#name');
+    this.selectCountryButton = page.locator('div:has(label[for="location"]) > button');
+    this.searchCountryInput = page.locator('input[placeholder="Search country..."]');
+    this.dropdownContainer = page.locator('div[data-side="bottom"]');
+    this.selectServicesButton = page.locator('div:has(label[for="services"]) > button');
+    this.searchServiceInput = page.locator('input[placeholder="Search country..."]');
+    this.searchPhoneButton = page.locator('div.PhoneInput  > button[aria-haspopup="dialog"]');
+  }
 
+  async createVendor(vendorData) {
+    await this.nameInput.fill(vendorData.name);
+    await this.selectCountryButton.click();
+    await this.searchCountryInput.fill(vendorData.location);
+    await this.dropdownContainer.locator('div.overflow-hidden > div span.text-sm', { hasText: new RegExp(`^${vendorData.location}$`, 'i') }).click();
+    await this.selectServicesButton.click();
+    /*
+    for (const service of vendorData.services) {
+      await this.searchServiceInput.fill(service);
+      await this.dropdownContainer
+        .locator('div.overflow-hidden > div[role="listbox"]', { hasText: new RegExp(`^${service}$`, 'i') })
+        .click();
+    }
+    */
+  }
+  async editVendor(vendorData) {
+    await this.nameInput.fill(`${vendorData.name} - Edited`);
+    await this.selectCountryButton.click();
+    await this.searchCountryInput.fill(vendorData.location);
+    await this.dropdownContainer.locator('div.overflow-hidden > span.text-sm', { hasText: new RegExp(`^${vendorData.location}$`, 'i') }).click();
+    await this.selectServicesButton.click();
+    /*
+    for (const service of vendorData.services) {
+      await this.selectServicesButton.click();
+      await this.searchServiceInput.fill(service);
+      await this.dropdownContainer
+        .locator('div.overflow-hidden > div[role="listbox"]', { hasText: new RegExp(`^${service}$`, 'i') })
+        .click();
+    }
+    */
   }
 }
--- e2e/tests/modules/vendors/vendors.spec.js
+++ e2e/tests/modules/vendors/vendors.spec.js
@@ -2,9 +2,33 @@ import { test, expect } from "@playwright/test";
 import { allure } from "allure-playwright";
 import { faker } from "@faker-js/faker";
 import { BasePage, VendorsPage } from "../../../pages";
+import { countires, services } from '../../../support/constants/constants';
+
+function getRandomServices(serviceList) {
+  let shuffled = serviceList.sort(() => 0.5 - Math.random());
+  let count = Math.floor(Math.random() * serviceList.length) + 1;
+  return shuffled.slice(0, count);
+};
+
+function generateCleanName() {
+  let fullName = faker.person.fullName();
+  return fullName.replace(/[^a-zA-Z\s]/g, "");
+}
 
 let basePage;
 let vendorsPage;
+let vendorData = {
+  name: generateCleanName(),
+  location: countires[Math.floor(Math.random() * countires.length)],
+  services: getRandomServices(services),
+  status: "active",
+  contactVendorName: generateCleanName(),
+  email: faker.internet.email().toLowerCase(),
+  phone: faker.phone.number("### ### ####"),
+  website: faker.internet.url(),
+  vendorDescription: faker.lorem.sentence(),
+  InternalNotes: faker.lorem.sentence(),
+};
 
 test.beforeEach(async ({ page, context }) => {
   await allure.epic("Vendors - Casos de prueba relacionados a la vista de Vendedores");
@@ -12,4 +36,14 @@ test.beforeEach(async ({ page, context }) => {
   await allure.tags("e2e", "vendors");
   basePage = new BasePage(page);
   vendorsPage = new VendorsPage(page);
+});
+
+
+test("@vendors - Verificación del flujo de Creación de un Proveedor nuevo", async ({ page }) => {
+  await allure.story('Se crea un Proovedor de forma Exitosa');
+  await allure.step(`Step 1 - Creación de un nuevo Proveedor`, async () => {
+    await page.goto(`${process.env.BASEURL}/vendors/create`);
+    await page.waitForURL("/vendors/create");
+    await vendorsPage.createVendor(vendorData);
+  });
 });
